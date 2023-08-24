11a5d69b2e33066b998abd55788edd752de4125f
kjh <kimjh4495@gmail.com>
2023-08-24 18:30:03 +0900

    .

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_rating/meal_api.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -12,23 +14,44 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var controller = TextEditingController();
   bool enabled = false;
-  List<Score> score = [];
   double rate = 0;
+  String now = '날짜를 선택하세요';
+  dynamic listView = Text('결과 출력 화면');
+  void showReview({required String evalDate}) {
+    var api = MealApi();
+    var result =  api.getReview(evalDate: evalDate);
+    setState(() {
+      listView = FutureBuilder(builder:(context, snapshot) {
+        if(snapshot.hasData) {
+          var data = snapshot.data;
+          return ListView.separated(itemBuilder: (context, index) {
+            return ListTile(leading: Text(data['index']['rating']), title: Text(data['index']['comment']),);
+          }, separatorBuilder: (context, index) {
+            return Divider();
+          }, itemCount: data.length);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },future: result,);
+    });
 
+  }
   @override
   Widget build(BuildContext context) {
-    var listView = ListView.separated(
-      itemCount: score.length,
-      separatorBuilder: (context, index) => const Divider(),
-      itemBuilder: (context, index) => ListTile(
-        leading: Text('${score[index].rate}'),
-        title: Text(score[index].comment),
-      ),
-    );
     return MaterialApp(
       home: Scaffold(
         body: Column(
           children: [
+            ElevatedButton(onPressed: () async{
+              var dt = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2023), lastDate: DateTime.now());
+              if(dt != null) {
+              var date = dt.toString().split(' ')[0]; //2023-08-08
+              setState(() {
+                now = date;
+              });
+              showReview(evalDate: date);
+              }
+            }, child: Text(now)),
             RatingBar.builder(
               initialRating: 3,
               minRating: 0.5,
@@ -68,37 +91,17 @@ class _MyAppState extends State<MyApp> {
                   ? () async {
                       var api = MealApi();
                       //2023-08-16 16:55:45
-                      var evalDate = DateTime.now().toString().split(' ')[0];
                       var res =
-                          await api.insert(evalDate, rate, controller.text);
-                      print(res);
-
-                      //----------------------------
-                      score.add(
-                        Score(
-                          rate: rate,
-                          comment: controller.text,
-                        ),
-                      );
-                      setState(() {
-                        listView;
-                        enabled = false;
-                      });
-                      print(score.length);
+                          await api.insert(now, rate, controller.text);
+                          showReview(evalDate: now);
                     }
                   : null,
               child: const Text('저장하기'),
-            ),
-            Expanded(child: listView),
+            ),Expanded(child: listView),
+      
           ],
         ),
       ),
     );
   }
-}
-
-class Score {
-  double rate;
-  String comment;
-  Score({required this.rate, required this.comment});
 }
